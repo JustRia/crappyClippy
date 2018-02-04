37a05b6ee107244614d39d74fe8a26b1c25dee21
Unknown <jsmitty1414@gmail.com>
2018-02-04 17:05:20 -0500

    Cannot recognize button in popup and save.js

---
+++ src/save.js
@@ -0,0 +1,25 @@
+var saveButton = document.querySelector("button.save");
+saveButton.addEventListener('click', save)
+function save() {
+    var targetWindow = null;
+    var tabCount = 0;
+    var storage = chrome.storage.local;
+    alert("save");
+    start();
+    function start() {
+        alert("start")
+        //Returns calls getTabs(currentWindow) where current Window has a tabs property
+        chrome.windows.getCurrent(true, getTabs);
+
+    }
+    function getTabs(win) {
+        alert("getTabs")
+        tabCount = win.tabs.length;
+        for (var i = 0; i < tabCount; i++) {
+            var tab = win.tab[i];
+            storage.set({ 'title': tab.title }, { 'url': tab.url }, function () {
+                alert(tab.title + 'saved')
+            });
+        }
+    }
+}
