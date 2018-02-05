86ff6e8c2a8b9fea3dbd5a04f50afa11f596a656
chang382 <changaaron286@gmail.com>
2018-02-05 11:52:39 -0500

    Minor syntax fixes
    Fixed minor syntax issues to get button to save to local storage

--- js/save.js
+++ js/save.js
@@ -1,23 +1,27 @@
-var saveButton = document.querySelector('button.save');
+var saveButton = document.querySelector('div.save');
 saveButton.addEventListener('click', save)
 function save() {
     var targetWindow = null;
     var tabCount = 0;
-    var storage = chrome.storage.local;
     alert("save");
     start();
     function start() {
-        alert("start")
+        alert("start");
+        const obj = {
+          "populate": true
+        };
         //Returns calls getTabs(currentWindow) where current Window has a tabs property
-        chrome.windows.getCurrent(true, getTabs);
+        chrome.windows.getCurrent(obj, getTabs);
 
     }
     function getTabs(win) {
-        alert("getTabs")
+        var storage = chrome.storage.local;
+        alert("getTabs");
         tabCount = win.tabs.length;
+        console.log(win.tabs.length);
         for (var i = 0; i < tabCount; i++) {
-            var tab = win.tab[i];
-            storage.set({ 'title': tab.title }, { 'url': tab.url }, function () {
+            var tab = win.tabs[i];
+            storage.set({ 'title': tab.title, 'url': tab.url }, function () {
                 alert(tab.title + 'saved')
             });
         }
