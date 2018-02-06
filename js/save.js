4483139005ca6cce03927d074b8cb455753cbb5d
chang382 <changaaron286@gmail.com>
2018-02-06 10:41:28 -0500

    Added Redirect to input set name
    Clicking "Save All Tabs" redirects to save.html where the user inputs a set name

--- js/save.js
+++ js/save.js
@@ -1,5 +1,5 @@
-var saveButton = document.querySelector('div.save');
-saveButton.addEventListener('click', save)
+var saveButton = document.querySelector('button.save');
+saveButton.addEventListener('click', save);
 function save() {
     var targetWindow = null;
     var tabCount = 0;
@@ -14,11 +14,13 @@ function save() {
     }
     function getTabs(win) {
         var storage = chrome.storage.local;
-        window.open(chrome.extension.getURL("popup.html"), "gc-popout-window", "width=348,height=654")
-        var setName = window.prompt("Rename your tab set if you want", "My Tab Set");
+        //window.open(chrome.extension.getURL("popup.html"), "gc-popout-window", "width=348,height=654")
+        //var setName = window.prompt("Rename your tab set if you want", "My Tab Set");
+        setName = document.querySelector("input#setName").value;
         if (setName == null) {
             return;
         }
+        console.log(setName);
         var set = {
             "name": setName,
             "tabs": []
@@ -28,8 +30,33 @@ function save() {
             var tab = win.tabs[i];
             set.tabs.push(tab);
         }
+        /*
         storage.set(set, function callback() {
             alert(set.tabs + " Saved");
-        })
+        })*/
+
+        //Saving sets to an array called data saved to local storage
+        storage.get(null, function(items) {
+            if (Object.keys(items).length > 0 && items.data) {
+                // The data array already exists, add to it the new server and nickname
+                items.data.push(set);
+            } else {
+              console.log("in here");
+              // The data array doesn't exist yet, create it
+              items.data = [set];
+            }
+            console.log(items);
+            // Now save the updated items using set
+            chrome.storage.local.set(items, function() {
+              console.log('Data successfully saved to the storage!');
+            });
+        });
+
+        //Wait before going back to popup.html or else data won't be saved.
+        setTimeout(backHome, 200);
+    }
+
+    function backHome() {
+      window.location.href = "../popup.html"
     }
 }
