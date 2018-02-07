52b3b5742e8279459582f24ba483904eeca5f225
claytonhenrylewis <claytonhenrylewis@gmail.com>
2018-02-07 17:43:17 -0500

    Add html page and some js to save some tabs into a set

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,12 @@
 var saveButton = document.querySelector('div.save');
+var saveSelectButton = document.querySelector('div.select-save');
 saveButton.addEventListener('click', saveRedirect);
+saveSelectButton.addEventListener('click', selectSaveRedirect);
 
 function saveRedirect() {
   window.location.href = "../save.html"
+}
+
+function selectSaveRedirect() {
+  window.location.href = "../selectSave.html"
 }
