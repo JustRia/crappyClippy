98b4d80092061f3a1d12483af6eaabeeedaf7057
chang382 <changaaron286@gmail.com>
2018-02-08 10:05:16 -0500

    Clicking a set sets it for removal

--- js/remove.js
+++ js/remove.js
@@ -1,5 +1,8 @@
 listSets();
 
+var select = document.querySelector('div.set');
+select.addEventListener('click', select);
+
 function createNode(element) {
     return document.createElement(element);
 }
@@ -14,9 +17,11 @@ function listSets() {
     console.log(items.data);
     items.data.map(function (set) {
       console.log(set);
-      let div = createNode("div");
-      div.className += " option";
-      div.innerHTML = set.name;
+      let div = createNode("div"),
+          p = createNode("p");
+      div.className += " option set";
+      p.innerHTML = set.name;
+      append(div, p);
       append(ul, div);
     });
   })
