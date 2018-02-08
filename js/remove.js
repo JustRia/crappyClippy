19e5baf658690555ad73d2e4bb75c5376fd54c85
chang382 <changaaron286@gmail.com>
2018-02-08 17:13:51 -0500

    Sets are removed from local storage upon button click

--- js/remove.js
+++ js/remove.js
@@ -1,7 +1,10 @@
 listSets();
-
-var select = document.querySelector('div.set');
-select.addEventListener('click', select);
+var removeButton = document.querySelector('button.remove');
+removeButton.addEventListener('click', remove);
+var backButton = document.querySelector('div.home');
+backButton.addEventListener('click', function callback() {
+  window.location.href = "../popup.html"
+});
 
 function createNode(element) {
     return document.createElement(element);
@@ -11,18 +14,69 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
+function toggleSelected(e) {
+    const li = e.target;
+    if (li.classList.contains("selected")) {
+        li.classList.remove("selected");
+    } else {
+        li.classList.add("selected");
+    }
+}
+
 function listSets() {
   var ul = document.querySelector("div.setList");
   chrome.storage.local.get(null, function callback(items) {
     console.log(items.data);
+    if (items.data.length == 0 || items.data == null) {
+      let div = createNode("div");
+      div.setAttribute("class", "set");
+      div.innerHTML = "No Sets Saved";
+      append(ul, div);
+      var button = document.querySelector("button.remove");
+      button.setAttribute("hidden", true);
+    }
     items.data.map(function (set) {
-      console.log(set);
-      let div = createNode("div"),
-          p = createNode("p");
-      div.className += " option set";
-      p.innerHTML = set.name;
-      append(div, p);
+      let div = createNode("div");
+      div.setAttribute("class", "option set");
+      div.innerHTML = set.name;
+      div.addEventListener('click', (e) => toggleSelected(e));
+      //append(div, p);
       append(ul, div);
     });
   })
+}
+
+function remove() {
+  var setCount = document.querySelector("div.setList").children.length;
+  var storage = chrome.storage.local;
+  storage.get(null, function (items) {
+      if (Object.keys(items).length > 0 && items.data && items.data.length > 0) {
+        var newData = [];
+        var selected = 0;
+        for (var i = 0; i < setCount; i++) {
+          var element = document.querySelector("div.setList").children[i];
+          if (element.classList.contains("selected")) {
+            newData = items.data.filter((e) => e.name !== element.textContent);
+            selected++;
+          }
+        }
+        if (newData.length == 0 && selected == 0) {
+          return;
+        }
+        items.data = newData;
+        console.log(items.data);
+      } else {
+          setTimeout(backHome, 200);
+      }
+      // Now save the updated items using set
+      chrome.storage.local.set(items, function () {
+          console.log('Data successfully saved to the storage!');
+      });
+
+      setTimeout(backHome, 200);
+  });
+
+  function backHome() {
+      window.location.href = "../popup.html"
+  }
 }
