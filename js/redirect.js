8d7dd4f397c4fa196f88601ef88b55333a6b2cd5
chang382 <changaaron286@gmail.com>
2018-02-08 19:13:49 -0500

    Added redirect to edits page
    Also removed old link to font-awesome.css

--- js/redirect.js
+++ js/redirect.js
@@ -4,12 +4,17 @@ saveButton.addEventListener('click', saveRedirect);
 saveSelectButton.addEventListener('click', selectSaveRedirect);
 
 var removeButton = document.querySelector('div.remove');
+var editButton = document.querySelector('div.edit');
 removeButton.addEventListener('click', removeRedirect);
+editButton.addEventListener('click', editRedirect);
 
 function saveRedirect() {
   window.location.href = "../save.html"
 }
 
+function editRedirect() {
+  window.location.href = "../edit.html";
+}
 
 function removeRedirect() {
   window.location.href = "../remove.html"
