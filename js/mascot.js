091992a614d4da3eb66329eff2078750ccf7b3ef
Ria <chocolodoro@gmail.com>
2018-03-04 16:08:49 -0500

    mascots cycle through now!

--- js/mascot.js
+++ js/mascot.js
@@ -1,10 +1,17 @@
 mascot();
 function mascot() {
+    var pics = ["../img/deWae.png","../img/gottaPepeFast.png","../img/placeholder_clippy.png","../img/crappyClippy_512x512.png"];
+    var iter = 0;
     document.getElementById('clippy').addEventListener('click', function (e) {
         var r = Math.floor(Math.random() * 100);
         console.log(r);
         if (r % 4 == 0) {
             window.open("https://microsoft-edge.en.softonic.com/");
+        }
+        document.getElementById('clippy').src=pics[iter];
+        iter++;
+        if (iter == 4) {
+            iter = 0;  
         }
     });
 }
